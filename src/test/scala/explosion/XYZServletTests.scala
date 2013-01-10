4f5ad4cb6dff0092d98196256f3443975dc9dc9f
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-10 11:40:54 -0500

    The default servlet needs to be named "default."
    We're going to change this so the framework does this for you.

--- src/test/scala/explosion/XYZServletTests.scala
+++ src/test/scala/explosion/XYZServletTests.scala
@@ -2,11 +2,11 @@ package explosion
 
 import org.scalatra.test.scalatest._
 import org.scalatest.FunSuite
-import org.eclipse.jetty.servlet.DefaultServlet
+import org.eclipse.jetty.servlet.{ServletHolder, DefaultServlet}
 
 class XYZServletTests extends ScalatraSuite with FunSuite {
   addServlet(classOf[XYZServlet], "/*")
-addServlet(new DefaultServlet, "/")
+  servletContextHandler.addServlet(new ServletHolder("default", new DefaultServlet), "/")
 
   test("simple get") {
     // this doesn't work yet!
