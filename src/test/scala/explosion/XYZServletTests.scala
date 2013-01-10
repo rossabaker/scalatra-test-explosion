a70039aaa4d2c6b474764df2b517bcb1dd77896c
Colin Yates <colin.yates@qficonsulting.com>
2013-01-10 14:01:28 +0000

    Added default servlet to test with no effect

--- src/test/scala/explosion/XYZServletTests.scala
+++ src/test/scala/explosion/XYZServletTests.scala
@@ -2,9 +2,11 @@ package explosion
 
 import org.scalatra.test.scalatest._
 import org.scalatest.FunSuite
+import org.eclipse.jetty.servlet.DefaultServlet
 
 class XYZServletTests extends ScalatraSuite with FunSuite {
   addServlet(classOf[XYZServlet], "/*")
+addServlet(new DefaultServlet, "/")
 
   test("simple get") {
     // this doesn't work yet!
